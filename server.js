7355f827187bca138e15cf10d9ddfe576f7556cd
Ismail <ismail.haabare@gmail.com>
2025-09-28 14:57:51 +0300

    implement user API endpoints for GET and POST requests

--- server.js
+++ server.js
@@ -1,9 +1,79 @@
 const http = require("http")
 
+const users = [
+  {
+    id: 1,
+    name: "Ismail Abdifadil",
+    className: "CA226",
+  },
+  {
+    id: 2,
+    name: "Mohamed Ali Omar",
+    className: "CA221",
+  },
+  {
+    id: 3,
+    name: "Sadak Macalin",
+    className: "CA221",
+  },
+]
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "application/json")
-  res.end(JSON.stringify({ success: true, message: "Welcome to the server" }))
+  if (req.url === "/" && req.method === "GET") {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "application/json")
+    return res.end(
+      JSON.stringify({ success: true, message: "Welcome to the server" })
+    )
+  } else if (req.url === "/api/users" && req.method === "GET") {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "application/json")
+    return res.end(JSON.stringify(users))
+  } else if (
+    req.url.match(/^\/api\/users\/([0-9]+)$/) &&
+    req.method === "GET"
+  ) {
+    const id = req.url.match(/^\/api\/users\/([0-9]+)$/)[1]
+    const user = users.find((user) => user.id.toString() === id)
+    
+    if (!user) {
+      res.statusCode = 404
+      res.setHeader("Content-Type", "application/json")
+      return res.end(
+        JSON.stringify({ success: false, message: "User not found." })
+      )
+    }
+    res.statusCode = 200
+    res.setHeader("Content-Type", "application/json")
+    return res.end(JSON.stringify({ success: true, data: user }))
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = []
+
+    req
+      .on("data", (chunk) => {
+        body.push(chunk)
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString()
+        const cleanedBody = JSON.parse(body)
+        const { id, name, className } = cleanedBody
+
+        if (!id || !name || !className) {
+          res.setHeader("Content-Type", "application/json")
+          return res.end(
+            JSON.stringify({
+              success: false,
+              message: "Please, fill all the fields. [id, name, and className]",
+            })
+          )
+        }
+
+        users.push(cleanedBody)
+        res.setHeader("Content-Type", "application/json")
+        return res.end(JSON.stringify(users))
+      })
+  } else if (req.url === "/api/users" && req.method === "PUT") {
+  }
 })
 
 const PORT = 5000
