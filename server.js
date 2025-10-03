e99645d87bf9fbd316bb9e68121723650e7ed495
Ismail Abdifadil <ismail.haabare@gmail.com>
2025-10-03 10:33:44 +0300

    refactor: standardize string quotes and improve response structure in user API

--- server.js
+++ server.js
@@ -1,89 +1,89 @@
-const http = require("http")
+const http = require('http')
 
 const users = [
   {
     id: 1,
-    name: "Ismail Abdifadil",
-    className: "CA226",
+    name: 'Ismail Abdifadil',
+    className: 'CA226',
   },
   {
     id: 2,
-    name: "Mohamed Ali Omar",
-    className: "CA221",
+    name: 'Mohamed Ali Omar',
+    className: 'CA221',
   },
   {
     id: 3,
-    name: "Sadak Macalin",
-    className: "CA221",
+    name: 'Sadak Macalin',
+    className: 'CA221',
   },
 ]
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === '/' && req.method === 'GET') {
     res.statusCode = 200
-    res.setHeader("Content-Type", "application/json")
+    res.setHeader('Content-Type', 'application/json')
     return res.end(
-      JSON.stringify({ success: true, message: "Welcome to the server" })
+      JSON.stringify({ success: true, message: 'Welcome to the server' })
     )
-  } else if (req.url === "/api/users" && req.method === "GET") {
+  } else if (req.url === '/api/users' && req.method === 'GET') {
     res.statusCode = 200
-    res.setHeader("Content-Type", "application/json")
-    return res.end(JSON.stringify(users))
+    res.setHeader('Content-Type', 'application/json')
+    return res.end(JSON.stringify({ success: true, data: users }))
   } else if (
     req.url.match(/^\/api\/users\/([0-9]+)$/) &&
-    req.method === "GET"
+    req.method === 'GET'
   ) {
     const id = req.url.match(/^\/api\/users\/([0-9]+)$/)[1]
     const user = users.find((user) => user.id.toString() === id)
 
     if (!user) {
       res.statusCode = 404
-      res.setHeader("Content-Type", "application/json")
+      res.setHeader('Content-Type', 'application/json')
       return res.end(
-        JSON.stringify({ success: false, message: "User not found." })
+        JSON.stringify({ success: false, message: 'User not found.' })
       )
     }
     res.statusCode = 200
-    res.setHeader("Content-Type", "application/json")
+    res.setHeader('Content-Type', 'application/json')
     return res.end(JSON.stringify({ success: true, data: user }))
-  } else if (req.url === "/api/users" && req.method === "POST") {
+  } else if (req.url === '/api/users' && req.method === 'POST') {
     let body = []
 
     req
-      .on("data", (chunk) => {
+      .on('data', (chunk) => {
         body.push(chunk)
       })
-      .on("end", () => {
+      .on('end', () => {
         body = Buffer.concat(body).toString()
         const cleanedBody = JSON.parse(body)
         const { id, name, className } = cleanedBody
 
         if (!id || !name || !className) {
-          res.setHeader("Content-Type", "application/json")
+          res.setHeader('Content-Type', 'application/json')
           return res.end(
             JSON.stringify({
               success: false,
-              message: "Please, fill all the fields. [id, name, and className]",
+              message: 'Please, fill all the fields. [id, name, and className]',
             })
           )
         }
 
         users.push(cleanedBody)
-        res.setHeader("Content-Type", "application/json")
-        return res.end(JSON.stringify(users))
+        res.setHeader('Content-Type', 'application/json')
+        return res.end(JSON.stringify({ success: true, data: users }))
       })
   } else if (
     req.url.match(/^\/api\/users\/([0-9]+)$/) &&
-    req.method === "PUT"
+    req.method === 'PUT'
   ) {
     const id = req.url.match(/^\/api\/users\/([0-9]+)$/)[1]
     let body = []
 
     req
-      .on("data", (chunk) => {
+      .on('data', (chunk) => {
         body.push(chunk)
       })
-      .on("end", () => {
+      .on('end', () => {
         body = Buffer.concat(body).toString()
         const cleanedBody = JSON.parse(body)
 
@@ -91,9 +91,9 @@ const server = http.createServer((req, res) => {
 
         if (!user) {
           res.statusCode = 404
-          res.setHeader("Content-Type", "application/json")
+          res.setHeader('Content-Type', 'application/json')
           return res.end(
-            JSON.stringify({ success: false, message: "User does not exist." })
+            JSON.stringify({ success: false, message: 'User does not exist.' })
           )
         }
 
@@ -101,9 +101,9 @@ const server = http.createServer((req, res) => {
         user.className = cleanedBody.className || user.className
 
         res.statusCode = 200
-        res.setHeader("Content-Type", "application/json")
+        res.setHeader('Content-Type', 'application/json')
         return res.end(
-          JSON.stringify({ success: true, message: "User updated", data: user })
+          JSON.stringify({ success: true, message: 'User updated', data: user })
         )
       })
   }
